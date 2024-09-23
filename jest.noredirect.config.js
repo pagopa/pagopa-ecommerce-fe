5fd4d5d4234cb08c1a57b4494905c396606984d2
scaminati-bv <simone.caminati@bvtech.com>
2024-09-23 16:58:30 +0200

    fix: change test name

--- jest.noredirect.config.js
+++ jest.noredirect.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   preset: "jest-puppeteer",
-  testRegex: "./no-redirect.integration.test\\.ts$",
+  testRegex: "./no-redirect.test\\.ts$",
   reporters: [
       'default',
       [ 'jest-junit', {
         outputDirectory: './test_reports',
-        outputName: 'ecommerce-no-redirect-integration-TEST.xml',
+        outputName: 'ecommerce-no-redirect-TEST.xml',
       } ]
     ]
   };
