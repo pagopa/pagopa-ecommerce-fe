f2bc30780b150684bd8c662c38c49f69e1cfa9bb
Dylan Tangredi <dylan.tangredi.ext@jakala.com>
2025-04-23 12:50:14 +0200

    [CHK-4065] feat(unit-tests): improve jest coverage config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,11 @@ module.exports = {
   testEnvironment: "jsdom",
   preset: "ts-jest",
   testMatch: [
-      "**/__tests__/**/*.(tsx|ts)"
+    "**/__tests__/**/*.{ts,tsx}"
+  ],
+  testPathIgnorePatterns: [
+    "/dist/",
+    "/node_modules/"
   ],
   reporters: [
     'default',
@@ -15,32 +19,35 @@ module.exports = {
       outputName: 'sonar-report.xml',
     } ]
   ],
-  collectCoverage: true,
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80
+  globals: {
+    jestSonar: {
+      sonar56x: true,
+      reportPath: "test_reports",
+      reportFile: "sonar-report.xml",
+      indent: 4
     }
   },
+  collectCoverage: true,
   collectCoverageFrom: [
-      "src/**/*.ts",
-      "src/**/*.tsx",
-      "!<rootDir>/src/index.tsx",
-      "!<rootDir>/src/__tests__/**/*",
-      "!<rootDir>/src/__integration_tests__/**/*",
-      "!<rootDir>/src/__mocks__/**/*"
+    "src/**/*.ts",
+    "src/**/*.tsx",
+    "!<rootDir>/src/index.tsx",
+    "!<rootDir>/src/**/__tests__/**/*",
+    "!<rootDir>/src/**/__integration_tests__/**/*",
+    "!<rootDir>/src/**/__mocks__/**/*"
   ],
   coveragePathIgnorePatterns: [
-    "node_modules",
+    "/node_modules/",
+    "/dist/",
     "test-config",
-    ".module.ts",
-    "<rootDir>/src/translations",
+    "\\.module\\.ts$",
+    "<rootDir>/src/translations/",
     "<rootDir>/src/generated/",
-    "<rootDir>/src/__mocks__/*.ts",
-    "index.tsx",
-    "global.d.ts",
+    "global\\.d\\.ts$"
   ],
-  testResultsProcessor: "jest-sonar-reporter"
+  coverageDirectory: "<rootDir>/coverage/",
+  coverageReporters: ["cobertura", "lcov", "html", "text"],
+  coverageThreshold: {
+    global: { branches: 80, functions: 80, lines: 80, statements: 80 }
+  }
 };
