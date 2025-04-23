e12efd12e44acebc9c022f5aaef75bc85473d190
Dylan Tangredi <dylan.tangredi.ext@jakala.com>
2025-04-23 10:29:49 +0200

    [CHK-4065] feat(unit-tests): improve jest coverage config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   testEnvironment: "jsdom",
   preset: "ts-jest",
-  testRegex: "./*unit.test\\.ts$",
+  testMatch: [
+      "**/__tests__/**/*.(tsx|ts)"
+  ],
   reporters: [
     'default',
     [ 'jest-junit', {
@@ -12,6 +14,33 @@ module.exports = {
       outputDirectory: './test_reports',
       outputName: 'sonar-report.xml',
     } ]
+  ],
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  collectCoverageFrom: [
+      "src/**/*.ts",
+      "src/**/*.tsx",
+      "!<rootDir>/src/index.tsx",
+      "!<rootDir>/src/__tests__/**/*",
+      "!<rootDir>/src/__integration_tests__/**/*",
+      "!<rootDir>/src/__mocks__/**/*"
+  ],
+  coveragePathIgnorePatterns: [
+    "node_modules",
+    "test-config",
+    ".module.ts",
+    "<rootDir>/src/translations",
+    "<rootDir>/src/generated/",
+    "<rootDir>/src/__mocks__/*.ts",
+    "index.tsx",
+    "global.d.ts",
   ],
   testResultsProcessor: "jest-sonar-reporter"
 };
