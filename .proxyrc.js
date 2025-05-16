5ccdec115b3ac6b2d5d5f8d7b68b3468ae90c213
ciuffagianluca <gianluca.ciuffa@pagopa.it>
2025-05-16 13:31:20 +0200

    rename proxy variable

--- .proxyrc.js
+++ .proxyrc.js
@@ -13,7 +13,7 @@ const apiHost = "http://127.0.0.1:8080";
 const apiHostIO = "http://127.0.0.1:8082";
 const ecommerceBasepathV1 = "/ecommerce/checkout/v1";
 const ecommerceBasepathV2 = "/ecommerce/checkout/v2";
-const ecommerceIOBasepathV2 = "/ecommerce/webview/v1";
+const ecommerceIOBasepathV1 = "/ecommerce/webview/v1";
 
 module.exports = function (app) {
     app.use(createProxyMiddleware(ecommerceBasepathV1, {
@@ -22,7 +22,7 @@ module.exports = function (app) {
     app.use(createProxyMiddleware(ecommerceBasepathV2, {
         target: apiHost,
     }));
-    app.use(createProxyMiddleware(ecommerceIOBasepathV2, {
+    app.use(createProxyMiddleware(ecommerceIOBasepathV1, {
         target: apiHostIO,
     }));
 
