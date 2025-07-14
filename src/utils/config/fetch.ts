6b9614ca8cc174899df08e2230bd4975ccd27ef1
Forciniti Luigi <luigi.forciniti.ext@jakala.com>
2025-07-14 17:26:29 +0200

    chore: refactoring

--- src/utils/config/fetch.ts
+++ src/utils/config/fetch.ts
@@ -80,5 +80,5 @@ export const constantPollingWithPromisePredicateFetch = (
     retryLogic
   );
 
-  return retriableFetch(retryWithPromisePredicate, shouldAbort)(timeoutFetch);
+  return retriableFetch(retryWithPromisePredicate, shouldAbort)(timeoutFetch as any);
 };
