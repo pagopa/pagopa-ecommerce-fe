4e5d8cab64340acce467bbbde9e0db8d4572186a
matteo-samoila-jakala <matteo.samoila@jakala.com>
2026-02-06 11:35:15 +0100

    fix: removed useless margin

--- src/routes/IframeCardPage.tsx
+++ src/routes/IframeCardPage.tsx
@@ -42,7 +42,7 @@ export default function IFrameCardPage() {
             id={"helpLink"}
             variant="text"
             onClick={() => setCvvModalOpen(true)}
-            sx={{ p: 0, height: "auto", minHeight: "auto", mt: 2 }}
+            sx={{ p: 0, height: "auto", minHeight: "auto"}}
           >
             {t("iframeCardPage.helpLink")}
           </Button>
