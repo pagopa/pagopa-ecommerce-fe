c79840e481f3ecdc77a97b9f581fedef336b0a26
ciuffagianluca <gianluca.ciuffa@pagopa.it>
2026-09-16 14:55:06 +0200

    remove unused func import

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ import { EcommerceRoutes } from "./routes/models/routeModel";
 import PaymentResponsePage from "./routes/PaymentResponsePage";
 import GdiCheckPage from "./routes/GdiCheckPage";
 import "./translations/i18n";
-import { getConfigOrThrow } from "./utils/config/config";
 import SaveCardPage from "./routes/SaveCardPage";
 
 const checkoutTheme = createTheme({
